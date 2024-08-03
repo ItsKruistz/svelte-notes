55aa475adbbe48fb630b3046187d6caada3ac1b6
ItsKruistz <lekruistz@gmail.com>
2024-08-04 06:28:20 +0700

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    svelte(),
+  ],
+  optimizeDeps: {
+    force: true,
+  },
+  resolve: {
+    alias: {
+      $lib: path.resolve('./src/lib'),
+    },
+  },
+});
